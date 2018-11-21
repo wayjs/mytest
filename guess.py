203716476cc93ffcc336a7e8829839e751ff7fd8
姚文林 <1076377207@qq.com>
2018-11-21 18:56:39 +0800

    在家第一次提交

---
+++ guess.py
@@ -0,0 +1,15 @@
+# Author:ways
+age_of_oldboy=56
+count=0
+while count<3:
+    guess_age=int(input("guess age:"))
+    if guess_age==age_of_oldboy:
+        print("you get it")
+        break
+    elif guess_age>age_of_oldboy:
+        print("think smaller")
+    else:
+        print("think bigger")
+    count += 1
+else:
+    print("you have tried too many times .... fuck off")
